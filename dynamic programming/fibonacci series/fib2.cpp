b31be56f19bb2e5b529cf7356fe2cfe7fe5c23cf
Eliana <39322110+elianalopez@users.noreply.github.com>
2021-11-07 06:12:33 -0500

    Update fib2.cpp

--- dynamic programming/fibonacci series/fib2.cpp
+++ dynamic programming/fibonacci series/fib2.cpp
@@ -3,14 +3,13 @@
 int fib2(int n) {
 
    int f[n];
-   int i;
 
    f[0] = 0;
    f[1] = 1;
 
-   for (i = 2; i < n; i++) {
-      f[i] = f[i-1] + f[i-2];
-   }
+   for (i = 2; i < n; i++) 
+      {  f[i] = f[i-1] + f[i-2]; }
+   
    return f[n-1];
 }
 
