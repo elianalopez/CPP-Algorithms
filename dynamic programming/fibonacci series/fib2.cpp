a1c693628c30c84a0efa82568d787cdb1168a922
Eliana <39322110+elianalopez@users.noreply.github.com>
2021-11-07 05:33:46 -0500

    Add files via upload

---
+++ dynamic programming/fibonacci series/fib2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int fib2(int n) {
+
+   int f[n];
+   int i;
+
+   f[0] = 0;
+   f[1] = 1;
+
+   for (i = 2; i < n; i++) {
+      f[i] = f[i-1] + f[i-2];
+   }
+   return f[n-1];
+}
+
+int main () {
+   int n = 10;
+   std::cout << fib2(n);
+   return 0;
+}
